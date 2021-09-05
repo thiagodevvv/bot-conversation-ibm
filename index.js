719e24c2a5045eb7c02b76bc0fc7654ba4ae0505
lemao-code <th.ferrari@outlook.com.br>
2021-09-05 13:07:25 -0300

    connect with assistant and send/receive message

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 /// Lets go use "  ; "
 
 require('dotenv').config();
+
 const cors = require('cors')
 const express = require('express');
 const app = express();
-
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const { IamAuthenticator } = require('ibm-watson/auth');
 const PORT =  8000;
 
 app.use(cors());
 app.use(express.json());
 
 app.post('/', (req,res) => {
-    console.log(req.body.text)
+    const assistant = new AssistantV2({
+        version: '2021-06-14',
+        authenticator: new IamAuthenticator({
+          apikey: process.env.IBM_API_KEY,
+        }),
+        serviceUrl: process.env.SERVICE_URL,
+        disableSslVerification: true,
+      });
+      assistant.createSession({
+          assistantId: process.env.ASSISTANT_ID
+      }).then(data => {
+          console.log(JSON.stringify(data.result.session_id));
+          assistant.message({
+              assistantId: process.env.ASSISTANT_ID,
+              sessionId: data.result.session_id,
+            //   input: {
+            //       'message_type': 'text',
+            //       'text': `${req.body.text}`
+            //   }
+          }).then((data) =>  {
+              return res.send(data.result);
+          }).catch(err => {
+              console.log(`Erro ao mandar/receber mensagem::: ${err}`);
+          })
+      }).catch(err => {
+          console.log(`Erro ao criar sessão: ${err}`);
+      })
 })
 app.listen(PORT, () => console.log(`Server started in port::: ${PORT}`));
 
