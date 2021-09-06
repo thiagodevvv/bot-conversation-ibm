bc0c6990de69e9dfc429899e988aad8f5505cc2d
lemao-code <th.ferrari@outlook.com.br>
2021-09-05 22:48:12 -0300

    change router for test in machine aws ec2

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ require('dotenv').config();
 const cors = require('cors')
 const express = require('express');
 const app = express();
+const router = require('./src/routes/index')
 const PORT =  8000;
 
 app.use(cors());
 app.use(express.json());
 
 
-app.use(require('./src/routes'))
+app.use('/', router)
 app.listen(PORT, () => console.log(`Server started in port::: ${PORT}`));
 
 
